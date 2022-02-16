fcae61abd01c582c27287099600a2a4e7dfafd3c
Panda64 <JaylenSchelb@yahoo.com>
2022-02-15 23:10:17 -0800

    added routes + finished main tutorial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
+import { Outlet } from 'react-router-dom'
+
 import './App.css';
 import Title from './Title';
-import POPOSList from './POPOSList';
-import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Title />
-      <POPOSList />
-      <Footer />
+      <Outlet />
     </div>
   );
 }
